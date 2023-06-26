cd65b2365f68990e981a6c988a7c9461ae6cfc6b
Captain_Kad <buckskad@gmail.com>
2023-06-26 16:06:03 -0400

    started assigning roles to different components and separating them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 import "./App.css";
-import WeatherCard from "./weatherCard";
+import WeatherCard from "./components/weatherCard/weatherCard";
 
 // A functional component (This component is prefered cause its more readable)
-const Intro = () => {
+const App = () => {
   return (
     <div className="App">
       <h1 className="header">Welcome to My Weather App</h1>
@@ -13,49 +13,9 @@ const Intro = () => {
         Hello World! Welcome to my very first react app where I make a weather
         app
       </p>
-    </div>
-  );
-};
-
-const Final = () => {
-  return (
-    <div className="main-section">
-      <Intro />
       <WeatherCard />
-      <br />
-      <h2>List of Countries:</h2>
-      <ul>
-        <li>Nigeria</li>
-        <li>Canada</li>
-        <li>USA</li>
-        <li>Canada</li>
-      </ul>
     </div>
   );
 };
 
-export default Final;
-
-// function Intro() {
-//   return (
-//     <div className="App">
-//       <h1 className="header">Welcome to React</h1>
-//       <p className="info">Hello World! Welcome to my very first React App</p>
-//     </div>
-//   );
-// };
-
-// function Final() {
-//   return (
-//     <div className="main-section">
-//       <Intro />
-//       <h2>Fruits:</h2>
-//       <ul>
-//         <li>Apples</li>
-//         <li>Blueberries</li>
-//         <li>Strawberries</li>
-//         <li>Bananas</li>
-//       </ul>
-//     </div>
-//   );
-// }
+export default App;
