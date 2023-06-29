232e69ac24fb265fec7eb80de586a46422a3d9b7
Captain_Kad <buckskad@gmail.com>
2023-06-29 18:56:32 -0400

    created to separate the WeatherCondition logic from the main file

---
+++ src/components/weatherCard/weatherCondition.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WeatherCondition = (props) => {
+  return (
+    <div className="weather-condition">
+      <h1 className="temp">30°C</h1>
+      <h3 className="condition">Sunny</h3>
+    </div>
+  );
+};
+
+export default WeatherCondition;
