5dc9490191bf3893616eb9ae0e6ee58a0c439f5c
Captain_Kad <buckskad@gmail.com>
2023-06-29 18:35:15 -0400

    created a new file just for the icon display logic

---
+++ src/components/weatherCard/icon.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Icon = (props) => {
+    return (
+      // Responsibility: Displays the appropriate icon
+    <div className="icon">
+      <img className="icon" src="" alt="Weather Icon" />
+    </div>
+  );
+};
+
+export default Icon;
