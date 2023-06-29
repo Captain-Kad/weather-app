801a313d27a7d82ccd5e407c84c7179c4b0f8ceb
Captain_Kad <buckskad@gmail.com>
2023-06-29 18:36:26 -0400

    formatted with prettier code

--- src/components/weatherCard/icon.js
+++ src/components/weatherCard/icon.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const Icon = (props) => {
-    return (
-      // Responsibility: Displays the appropriate icon
+  return (
+    // Responsibility: Displays the appropriate icon
     <div className="icon">
       <img className="icon" src="" alt="Weather Icon" />
     </div>
