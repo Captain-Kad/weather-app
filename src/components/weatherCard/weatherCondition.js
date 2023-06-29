648fbb1d5aaacc04c20b85877f746bf6983e86e7
Captain_Kad <buckskad@gmail.com>
2023-06-29 19:01:38 -0400

    formatted with prettier code

--- src/components/weatherCard/weatherCondition.js
+++ src/components/weatherCard/weatherCondition.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const WeatherCondition = (props) => {
-    return (
-      // Responsibility: Handles the changing of the weather condition
+  return (
+    // Responsibility: Handles the changing of the weather condition
     <div className="weather-condition">
       <h1 className="temp">30°C</h1>
       <h3 className="condition">Sunny</h3>
