1df25fe98e702a2d84a9f917b6946d0339552724
Captain_Kad <buckskad@gmail.com>
2023-07-04 20:55:48 -0400

    moved styles from app.css to weathercondition.js

--- src/components/weatherCard/weatherCondition.js
+++ src/components/weatherCard/weatherCondition.js
@@ -1,11 +1,23 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 const WeatherCondition = (props) => {
+  const Temp = styled.h1`
+    font-family: "Fira Sans", sans-serif;
+    font-size: 2rem;
+    font-weight: 200;
+  `;
+
+  const Condition = styled.h3`
+    font-family: "Merriweather", sans-serif;
+    font-size: 1.2rem;
+  `;
+
   return (
     // Responsibility: Handles the changing of the weather condition
     <div className="weather-condition">
-      <h1 className="temp">30°C</h1>
-      <h3 className="condition">Sunny</h3>
+      <Temp>30°C</Temp>
+      <Condition>Sunny</Condition>
     </div>
   );
 };
