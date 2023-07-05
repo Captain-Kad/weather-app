a714ed8ac485fcf09d8bf91aa7afecfd186bc1d3
Captain_Kad <buckskad@gmail.com>
2023-07-05 01:29:22 -0400

    moved the style and updated the logic for the changing of the weather card

--- src/components/weatherCard/weatherCard.js
+++ src/components/weatherCard/weatherCard.js
@@ -1,66 +1,58 @@
 import React from "react";
-import { styled } from "@emotion/styled";
+import styled from "@emotion/styled";
 
 import Location from "./location";
 import Icon from "./icon";
 import WeatherCondition from "./weatherCondition";
 
 // changes the background color in the card
-// gets the root element from the css file which provides access to the variables created in the css file
-const root = document.querySelector(":root");
 
 const WeatherCard = (props) => {
-  //Declaration and defining of variables
   let temp = props.temp;
-  root.style.setProperty("--temp", temp);
-
   let highColor = 0;
-  root.style.setProperty("--highColor", highColor);
-
   let lowColor = 0;
-  root.style.setProperty("--lowColor", lowColor);
-
   let highNum = null;
-  root.style.setProperty("--highNum", highNum);
-
   let lowNum = null;
-  root.style.setProperty("--lowNum", lowNum);
 
-  if (props.temp > 12) {
+  if (temp > 12) {
     //Logic for hot weather
     highNum = 255;
-    root.style.setProperty("--highNum", highNum);
-
     lowNum = 0;
-    root.style.setProperty("--lowNum", lowNum);
 
     highColor = (1 - (temp - 12) / 28) * 255;
-    root.style.setProperty("--highColor", highColor);
-
     lowColor = highColor - 150;
-    root.style.setProperty("--lowColor", lowColor);
-  } else if (props.temp <= 12) {
+  } else if (temp <= 12) {
     //Logic for cold weather
     highNum = 0;
-    root.style.setProperty("--highNum", highNum);
-
     lowNum = 255;
-    root.style.setProperty("--lowNum", lowNum);
 
     highColor = (1 - (temp + 20) / 32) * 255;
-    root.style.setProperty("--highColor", highColor);
-
     lowColor = highColor - 150;
-    root.style.setProperty("--lowColor", lowColor);
   }
+
+  const Card = styled.div`
+    margin: 0 auto;
+    background: linear-gradient(
+      to top,
+      rgb(${highNum}, ${highColor}, ${lowNum}),
+      rgb(${highNum}, ${lowColor}, ${lowNum})
+    );
+    width: 200px;
+    height: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 15px;
+  `;
 
   return (
     // Responsibility: To display things and change the background of the card based on a variable passed to it
-    <div className="card">
+    <Card>
       <Location />
       <Icon />
       <WeatherCondition />
-    </div>
+    </Card>
   );
 };
 
