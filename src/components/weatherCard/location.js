2dc4c6d7f3e685507f2d082ee0a7e71fde2decb5
Captain_Kad <buckskad@gmail.com>
2023-07-04 20:24:53 -0400

    moved styles from app.css to location.js

--- src/components/weatherCard/location.js
+++ src/components/weatherCard/location.js
@@ -1,12 +1,16 @@
 import React from "react";
+import styled from "@emotion/styled";
 
 const Location = (props) => {
+  const Container = styled.div`
+    text-align: center;
+  `;
   return (
     // Responsibility: To change Location
-    <div className="location">
+    <Container>
       <h1 className="city">Lagos</h1>
       <h3 className="country">NG</h3>
-    </div>
+    </Container>
   );
 };
 
