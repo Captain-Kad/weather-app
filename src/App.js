7b4f652f910d7d57caeb5e49400412acb074fdad
Captain_Kad <buckskad@gmail.com>
2023-07-09 03:02:12 -0400

    created an asyc function to get data from the weather api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import WeatherCard from "./components/weatherCard/weatherCard";
 
 // A functional component (This component is prefered cause its more readable)
 const App = () => {
+  const data = async () => {
+    const apiResponse = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=89b273a95685d5c09e406eaee964c126"
+    );
+    const resJSON = await apiResponse.json();
+    return resJSON;
+  };
+
+  data().then((res) => console.log(res));
+
   return (
     <div className="App">
       <h1 className="header">Welcome to My Weather App</h1>
