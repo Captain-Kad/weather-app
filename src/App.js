f6182864447e22bfb1b1289f6771182dc01a61d9
Captain_Kad <buckskad@gmail.com>
2023-07-09 23:25:35 -0400

    updated the file so it takes input from the open weather api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import WeatherCard from "./components/weatherCard/weatherCard";
 
 // A functional component (This component is prefered cause its more readable)
 const App = () => {
-  const data = async () => {
+  // hooks
+  const [query, setQuery] = useState("Lagos, ng");
+  const [weather, setWeather] = useState({
+    city: null,
+    country: null,
+    temp: null,
+    condition: null,
+  });
+
+  const data = async (q) => {
     const api_key = process.env.REACT_APP_NEW_WEATHER_API_KEY;
     const apiResponse = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&APPID=${api_key}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=${api_key}`
     );
     const resJSON = await apiResponse.json();
     return resJSON;
   };
 
-  data().then((res) => console.log(res));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data(query).then((res) => {
+      setWeather({
+        city: res.name,
+        country: res.sys.country,
+        temp: res.main.temp,
+        condition: res.weather[0].main,
+      });
+    });
+  };
+
+  // Preloaded values so the card isn't empty
+  useEffect(() => {
+    data(query).then((res) => {
+      setWeather({
+        city: res.name,
+        country: res.sys.country,
+        temp: res.main.temp,
+        condition: res.weather[0].main,
+      });
+    });
+  });
 
   return (
     <div className="App">
@@ -24,9 +55,18 @@ const App = () => {
         Hello World! Welcome to my very first react app where I make a weather
         app
       </p>
-      <WeatherCard city="Lagos" country="NG" temp={30} condition="Dust" />
+      <WeatherCard
+        city={weather.city}
+        country={weather.country}
+        temp={weather.temp}
+        condition={weather.condition}
+      />
       <WeatherCard city="London" country="CA" temp={10} condition="Clouds" />
       <WeatherCard city="Manchester" country="UK" temp={18} condition="Clear" />
+      <form>
+        <input value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={(e) => handleSearch(e)}>Search</button>
+      </form>
     </div>
   );
 };
