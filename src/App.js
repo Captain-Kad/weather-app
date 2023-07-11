855b5e79d364484cf4971f74ac3c7f80cf1b0300
Captain_Kad <buckskad@gmail.com>
2023-07-11 17:56:57 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ import WeatherEngine from "./components/weatherEngine";
 const App = () => {
   return (
     <div className="App">
-      <h1 className="header">Weather App</h1>
-      <hr />
-      <p className="info">
-        Hello World! Welcome to my very first react app where I make a weather
-        app
-      </p>
       <WeatherEngine location={"lagos, ng"} />
     </div>
   );
