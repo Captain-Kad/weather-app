8b4e3769c8a649a6b2f338265e74a2c7789ee1ef
Captain_Kad <buckskad@gmail.com>
2023-07-11 00:34:25 -0400

    updated files

--- src/components/weatherCard/weatherCondition.js
+++ src/components/weatherCard/weatherCondition.js
@@ -2,17 +2,6 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const WeatherCondition = ({ temp, condition }) => {
-  const Temp = styled.h1`
-    font-family: "Fira Sans", sans-serif;
-    font-size: 2rem;
-    font-weight: 200;
-  `;
-
-  const Condition = styled.h3`
-    font-family: "Merriweather", sans-serif;
-    font-size: 1.2rem;
-  `;
-
   return (
     // Responsibility: Handles the changing of the weather condition
     <div className="weather-condition">
@@ -23,3 +12,14 @@ const WeatherCondition = ({ temp, condition }) => {
 };
 
 export default WeatherCondition;
+
+const Temp = styled.h1`
+  font-family: "Fira Sans", sans-serif;
+  font-size: 2rem;
+  font-weight: 200;
+`;
+
+const Condition = styled.h3`
+  font-family: "Merriweather", sans-serif;
+  font-size: 1.2rem;
+`;
