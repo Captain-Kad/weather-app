b819ec48dd1448e6396f1c36851a4e592824d102
Captain_Kad <buckskad@gmail.com>
2023-07-14 17:39:59 -0400

    created a file to handle the styling of the page title

---
+++ src/components/weatherCard/appTitle.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Header = styled.div``;
+
+const Title = styled.h1``;
+
+const AppTitle = () => {
+  return (
+    <Header>
+      <Title>Weather App</Title>
+    </Header>
+  );
+};
+
+export default AppTitle;
