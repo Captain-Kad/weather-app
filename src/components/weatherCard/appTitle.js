caa0f5ed0fe3dc718d7ba9ca092e2145c178ac1d
Captain_Kad <buckskad@gmail.com>
2023-07-14 18:43:01 -0400

    updated file

--- src/components/weatherCard/appTitle.js
+++ src/components/weatherCard/appTitle.js
@@ -1,15 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Header = styled.div`
-  padding-top: 30px;
-  padding-bottom: 30px;
-`;
-
-const Title = styled.h1`
-  font-size: 50px;
-`;
-
 const AppTitle = () => {
   return (
     <Header>
@@ -19,3 +10,12 @@ const AppTitle = () => {
 };
 
 export default AppTitle;
+
+const Header = styled.div`
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
+
+const Title = styled.h1`
+  font-size: 50px;
+`;
