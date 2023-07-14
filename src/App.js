62d6efd599a040a936a9b01d22f304de7561f7da
Captain_Kad <buckskad@gmail.com>
2023-07-14 17:56:02 -0400

    made elements line up vertically instead of horizontally

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./App.css";
 import WeatherEngine from "./components/weatherEngine";
+import AppTitle from "./components/weatherCard/appTitle";
 
 // A functional component (This component is prefered cause its more readable)
 const App = () => {
   return (
     <div className="App">
+      <AppTitle />
       <WeatherEngine location={"lagos, ng"} />
     </div>
   );
