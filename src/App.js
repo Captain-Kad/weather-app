9e11c7d8e5fa6d0bbe44f3f4864c38b034d0d142
Captain_Kad <buckskad@gmail.com>
2023-07-14 21:10:57 -0400

    updated file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import WeatherEngine from "./components/weatherEngine";
 import AppTitle from "./components/weatherCard/appTitle";
+import AppInfo from "./components/weatherCard/appInfo";
 
 // A functional component (This component is prefered cause its more readable)
 const App = () => {
   return (
     <div className="App">
       <AppTitle />
+      <AppInfo />
       <WeatherEngine location={"lagos, ng"} />
     </div>
   );
